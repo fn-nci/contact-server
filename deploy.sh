f5606c46da096dc529992d30677ee5d7bf9d44a3
Ailsa Prideaux-Mooney <x23283700@student.ncirl.ie>
2025-04-02 17:08:10 +0100

    mapping ports inside docker

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ then
 fi
 
 #create a container called node_app that is available on port 8444 from our docker image
-docker create -p 8444:8444 --name $CONTAINER_NAME $IMAGE_NAME
+docker create -p 80:8080 -p 8443:8444 --name $CONTAINER_NAME $IMAGE_NAME
 #write the private key to a file
 echo $PRIVATE_KEY > privatekey.pem
 #write the server key to a file
