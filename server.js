c10c1ae76daa8853ed8f0c6abd3240f3a1c14df7
Flavio Nunes <flavio.nunes@mapall.com>
2025-04-03 19:29:39 +0100

    Fix: Server IP

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const db = require('./database');
 
 const app = express();
-const PORT = process.env.PORT || 8443;
+const PORT = process.env.PORT || 4000;
 const csrfProtection = csrf({ cookie: true });  // initialize CSRF protection middleware w/ cookie-based tokens
 
 //allow requests from frontend and allow cookies
